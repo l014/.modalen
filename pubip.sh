a404da1af4c8ddf8cd809e2836c17bc55ac48396
l014 <74553087+l014@users.noreply.github.com>
2021-06-16 12:59:41 +0000

    Create pubip.sh
    output
    ```
    using : https://ovpn.com/ https://ipv4.am.i.mullvad.net/ https://ipv6.am.i.mullvad.net/
    217.64.148.118
    217.64.148.118
    2a07:a880:4601:1155:c7f0:8d89:883e:e4bd
    ```

---
+++ pubip.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# 	display public ipv4/ipv6 using curl & egrep. ovpn.com & mullvad.net to resolve ip(s). ~ imo trust in your VPN provider is crusial 
+#	 the regxp should work on other domains \_(O-o)_/ - output looks ok using; ser="https://ipnr.nu/ https://www.dnsleaktest.com/ https://ip.me/"
+
+ser="https://ovpn.com/ https://ipv4.am.i.mullvad.net/ https://ipv6.am.i.mullvad.net/"  
+echo "using : "$ser;
+echo $ser| xargs curl -s -L -1  |egrep -E -o '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)||(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))'
